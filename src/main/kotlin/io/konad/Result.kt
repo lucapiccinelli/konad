2977c321be338e4be5ca01d5d0a3bfaf48bd0741
Luca Piccinelli <luca.picci@gmail.com>
2020-12-07 17:04:13 +0100

    result monadic and applicative behaviour

--- src/main/kotlin/io/konad/Result.kt
+++ src/main/kotlin/io/konad/Result.kt
@@ -1,12 +1,34 @@
 package io.konad
 
+import io.konad.exceptions.ResultException
+
 sealed class Result<out T>{
 
     data class Ok<T>(val value: T): Result<T>()
     data class Error(val description: String): Result<Nothing>()
 
+    companion object{
+        fun <T> pure(value: T) = Ok(value)
+    }
+
+    inline fun <R> map(fn: (T) -> R): Result<R> = flatMap { Ok(fn(it)) }
+
+    inline fun <R> flatMap(fn: (T) -> Result<R>): Result<R> = when(this){
+        is Ok -> fn(this.value)
+        is Error -> this
+    }
+
+    fun <R> ap(liftedFn: Result<(T) -> R>): Result<R> = liftedFn.flatMap { map(it) }
+
     fun get(): T = when(this){
         is Ok -> value
-        is Error -> throw io.konad.ResultException(this)
+        is Error -> throw ResultException(this)
     }
+}
+
+fun <T> Result<T>.ifError(defaultValue: T) = ifError { defaultValue }
+
+fun <T> Result<T>.ifError(errorHandler: (Result.Error) -> T) = when(this){
+    is Result.Ok -> value
+    is Result.Error -> errorHandler(this)
 }
--- src/test/kotlin/io/konad/ResultTests.kt
+++ src/test/kotlin/io/konad/ResultTests.kt
@@ -8,16 +8,63 @@ import io.kotest.matchers.shouldBe
 class ResultTests: StringSpec({
 
     "If the result is OK, when i get the result, then it returns the value" {
-        val x = Result.Ok(1)
+        val x: Result<Int> = Result.Ok(1)
         x.get() shouldBe 1
     }
 
     "If the result is Error, when i get the result, then it throws an Error" {
-        val x = Result.Error("error")
-        shouldThrow<ResultException> {
-            x.get()
-        }
+        val x: Result<Int> = Result.Error("error")
+        shouldThrow<ResultException> { x.get() }
     }
 
+    "If the result is OK, when i get the result or default, then it returns the value" {
+        val x: Result<Int> = Result.Ok(1)
+        x.ifError(2) shouldBe 1
+    }
+
+    "If the result is Error, when i get the result or default, then it returns the default" {
+        val x: Result<Int> = Result.Error("bla")
+        x.ifError(2) shouldBe 2
+    }
+
+    "Mapping an Ok result, when i get the result, i get the mapped result " {
+        val x: Result<Int> = Result.Ok(1).map { x -> x + 1 }
+        x.get() shouldBe 2
+    }
+
+    "Mapping an Error result, when i get the result, then it throws an Error" {
+        val x: Result<Int> = Result.Error("error").map { x: Int -> x + 1 }
+        shouldThrow<ResultException> { x.get() }
+    }
+
+    "FlatMapping an Ok result, when i get the result, i get the mapped result " {
+        val x: Result<Int> = Result.Ok(1).flatMap { x -> Result.Ok(x + 1) }
+        x.get() shouldBe 2
+    }
+
+    "FlatMapping an Error result, when i get the result, then it throws an Error" {
+        val x: Result<Int> = Result.Error("error").flatMap { x: Int -> Result.Ok(x + 1) }
+        shouldThrow<ResultException> { x.get() }
+    }
+
+    "FlatMapping an Ok result with a function that returns an error, when i get the result, then it throws an Error" {
+        val x: Result<Int> = Result.Ok(1).flatMap { x: Int -> Result.Error("banana") }
+        val ex = shouldThrow<ResultException> { x.get() }
+        ex.error.description shouldBe "banana"
+    }
+
+    "FlatMapping an Error result with a function that returns an error, when i get the result, then it throws the first Error" {
+        val x: Result<Int> = Result.Error("error1").flatMap { x: Int -> Result.Error("banana") }
+        val ex = shouldThrow<ResultException> { x.get() }
+        ex.error.description shouldBe "error1"
+    }
+
+    "A lifted function can be applied" {
+        val liftedFn = Result.pure { x: Int -> x + 1 }
+        val y: Result<Int> = Result.pure(1).ap(liftedFn)
+
+        y.get() shouldBe 2
+    }
 })
+
 
