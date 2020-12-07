de17d84bb17473a5fe4c94271ddb1904a0ae70e0
Luca Piccinelli <luca.picci@gmail.com>
2020-12-07 15:36:28 +0100

    repo initialization

---
+++ src/main/kotlin/com/pcc/Hello.kt
@@ -0,0 +1,6 @@
+package com.pcc
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/com/pcc/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.pcc
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
