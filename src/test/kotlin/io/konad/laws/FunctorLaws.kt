c1bf94abf77233f2880daf4db36f322d40701044
Luca Piccinelli <luca.picci@gmail.com>
2020-12-26 23:42:01 +0100

    generalized functor laws

---
+++ src/test/kotlin/io/konad/laws/FunctorLaws.kt
@@ -0,0 +1,27 @@
+package io.konad.laws
+
+import io.konad.*
+import io.konad.Maybe.Companion.maybe
+import io.konad.hkt.FunctorKind
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.property.checkAll
+import io.konad.Result
+
+open class FunctorLaws<F>(private val pure: (Double) -> FunctorKind<F, Double>): StringSpec({
+
+    "first functor law (identity)"{
+        val id = { x: Double -> x }
+        checkAll<Double> { v -> pure(v).mapK(id) shouldBe pure(v) }
+    }
+
+    "second functor law (composition)"{
+        val f = { x: Double -> x + 6 }
+        val g = { y: Double -> y * y }
+        checkAll<Double> { v -> pure(v).mapK(f).mapK(g) shouldBe pure(g(f(v))) }
+    }
+
+})
+
+class MaybeFunctorLaws(): FunctorLaws<MaybeOf>(Maybe.Companion::pure)
+class ResultFunctorLaws(): FunctorLaws<ResultOf>(Result.Companion::pure)
