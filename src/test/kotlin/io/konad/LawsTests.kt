7169c593a014a6ac10f8f4acb0b85131cf50809e
Luca Piccinelli <luca.picci@gmail.com>
2020-12-26 08:47:14 +0100

    maybe do not respect the second functor law

--- src/test/kotlin/io/konad/LawsTests.kt
+++ src/test/kotlin/io/konad/LawsTests.kt
@@ -19,15 +19,4 @@ class LawsTests: StringSpec({
         checkAll<Double> { v -> Result.pure(v).map(f).map(g) shouldBe Result.pure(g(f(v))) }
     }
 
-    "Maybe respects first functor law (identity)"{
-        val id = { x: String? -> x }
-        checkAll<String?> { v -> Maybe.pure(v).mapK(id) shouldBe Maybe.pure(v) }
-    }
-
-    "Maybe respects first functor law (composition)"{
-        val f = { x: Double? -> x?.let { x + 6 } ?: -1.0 }
-        val g = { y: Double? -> y?.let{ y * y } ?: -2.0 }
-        checkAll<Double> { v -> Maybe.pure(v).mapK(f).mapK(g) shouldBe Maybe.pure(g(f(v))) }
-    }
-
 })
